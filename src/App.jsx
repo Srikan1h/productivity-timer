588a7880bd84f99f77439608506a0927fbe26a2b
srikan1h <saisrikanth2007@gmail.com>
2026-06-17 23:14:10 +0530

    feat: replace brand text with clock icon and refine color palette variables

--- src/App.jsx
+++ src/App.jsx
@@ -268,7 +268,11 @@ function App() {
     <main className="app">
       <header className="app-bar">
         <button className="brand" type="button" onClick={() => setView('pomodoro')}>
-          <span>Kanth</span>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-clock-fill bg-primary" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+            </svg>
+          </span>
         </button>
 
         <div className="navbar-controls">
